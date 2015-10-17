bf68916a76927483f5b4f314b3afe19c2d50c1a8
willowcheng <e.wingus@Gmail.com>
2015-10-17 17:10:54 -0400

    Refactor product tabs

--- app.js
+++ app.js
@@ -8,18 +8,6 @@
         this.products = gems;
     });
 
-    app.controller('TabController', function(){
-        this.tab = 1;
-
-        this.setTab = function(newValue){
-            this.tab = newValue;
-        };
-
-        this.isSet = function(tabName){
-            return this.tab === tabName;
-        };
-    });
-
     app.controller('GalleryController', function(){
         this.current = 0;
         this.setCurrent = function(newGallery){
@@ -47,6 +35,25 @@
         return {
             restrict: 'A',
             templateUrl: 'product-specs.html'
+        };
+    });
+
+    app.directive("productTabs", function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'product-tabs.html',
+            controller: function () {
+                this.tab = 1;
+
+                this.isSet = function(checkTab) {
+                    return this.tab === checkTab;
+                };
+
+                this.setTab = function(setTab) {
+                    this.tab = setTab;
+                };
+            },
+            controllerAs: 'tab'
         };
     });
 
