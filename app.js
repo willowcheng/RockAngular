62ac6ef874390438b44a6f86cfef85c8f267d289
willowcheng <e.wingus@Gmail.com>
2015-10-18 01:14:20 -0400

    Built in Angular service (RESTful)

--- app.js
+++ app.js
@@ -4,9 +4,14 @@
 (function() {
     var app = angular.module('gemStore', ['store-directives']);
 
-    app.controller('StoreController', function(){
-        this.products = gems;
-    });
+    app.controller('StoreController', ['$http', function($http){
+        var store = this;
+        store.products = [];
+
+        $http.get('/store-products.json').success(function (data) {
+            store.products = data;
+        });
+    }]);
 
     app.controller('ReviewController', function(){
         this.review = {};
