9153d72a9061d3380439d935fe1e1b710372b2df
willowcheng <e.wingus@Gmail.com>
2015-10-18 01:03:39 -0400

    Refactor into a module

--- app.js
+++ app.js
@@ -2,24 +2,10 @@
  * Created by willowcheng on 15-09-13.
  */
 (function() {
-    var app = angular.module('gemStore', []);
+    var app = angular.module('gemStore', ['store-directives']);
 
     app.controller('StoreController', function(){
         this.products = gems;
-    });
-
-    app.directive('productGallery', function () {
-        return {
-            restrict: 'E',
-            templateUrl: 'product-gallery.html',
-            controller: function () {
-                this.current = 0;
-                this.setCurrent = function(newGallery){
-                    this.current = newGallery || 0;
-                };
-            },
-            controllerAs: 'gallery'
-        };
     });
 
     app.controller('ReviewController', function(){
@@ -28,39 +14,6 @@
             this.review.createdOn = Date.now();
             product.reviews.push(this.review);
             this.review = {};
-        };
-    });
-
-    app.directive('productDescription', function(){
-        return {
-            restrict: 'E',
-            templateUrl: 'product-description.html'
-        };
-    });
-
-    app.directive("productSpecs", function() {
-        return {
-            restrict: 'A',
-            templateUrl: 'product-specs.html'
-        };
-    });
-
-    app.directive("productTabs", function () {
-        return {
-            restrict: 'E',
-            templateUrl: 'product-tabs.html',
-            controller: function () {
-                this.tab = 1;
-
-                this.isSet = function(checkTab) {
-                    return this.tab === checkTab;
-                };
-
-                this.setTab = function(setTab) {
-                    this.tab = setTab;
-                };
-            },
-            controllerAs: 'tab'
         };
     });
 
